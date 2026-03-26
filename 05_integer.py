84ae9699cdcf6f6e5c2758692869eb8f55546987
Armaan Aquib <armaanaquib1@gmail.com>
2026-03-26 16:14:58 +0530

    int --> data type, function

---
+++ 05_integer.py
@@ -0,0 +1,42 @@
+
+
+x = input("Enter x:")
+y = input("Enter y:")
+
+z = x+y
+
+print(z)
+
+
+"""
+here the result will be if x = 1, y = 2 then z = 12 not equal to 3
+beacuse the input() function return 'str'
+"""
+
+#-----------------------------------------------------------------------------------
+
+x = input("Enter x:")
+y = input("Enter y:")
+
+z = int(x)+int(y)
+
+print(z)
+
+"""
+here, if x=1 and y=2 then --> z=3
+"""
+
+
+#---------------------------------------------------------------------------------------
+x = int(input("Enter x:"))
+y = int(input("Enter y:"))
+
+print(x+y)
+#-------------------------------------------------------------------------------------
+
+x,y = int(input("Enter x:")),int(input("Enter y:"))
+
+print(x+y)
+#------------------------------------------------------------------------------------
+
+
